387556653fd11c0b7945f6b5def8f0f82e4ab1a0
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-06-02 07:50:51 +0700

    add file header adt stack

---
+++ adtstack.h
@@ -0,0 +1,13 @@
+#ifndef ADTSTACK_H
+#define ADTSTACK_H
+
+#include "C:\SHARING\POLBAN SEMESTER 2\STRUKTUR DATA DAN ALGORITMA\ADT3\adt\linked_list.h"
+
+// Prototipe fungsi Stack
+void Push(address *top, int data);
+int Pop(address *top);
+void Tampil_Stack(address top);
+int Is_Empty(address top);
+void KonversiDesimalKeBiner(int desimal);
+
+#endif
